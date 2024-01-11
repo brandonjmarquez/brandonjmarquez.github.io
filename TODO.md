4b5a7b51db8df0fca0de5c959e7e166e531d718d
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2024-01-10 23:02:42 -0600

    Added TODO.md and updated README.md. Embedded a soundcloud playlist into AboutMe.jsx.

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# Portfolio
+
+### TODO
+- [ ] Animate project containers on hover.  
+- [ ] Animate buttons better.  
+- [ ] Add an infinite carousel of all the technologies I know.  
+- [ ] chatbot?  
+
+### Completed
+- [x] Embed soundcloud playlist into about me page.  
--- src/tabs/AboutMe.jsx
+++ src/tabs/AboutMe.jsx
@@ -38,6 +38,16 @@ const AboutMe = (props) => {
             <li className="text-start">I make music!(Here"s my <a href="https://soundcloud.com/user-550415450">soundcloud</a> if you want to check it out)</li>
             <li className="text-start">I"ve been coding for about 7 years now, and I"m not stopping any time soon.</li>
           </ul>
+          <iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1754747808&color=%23bd00ff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+          <div style={{fontSize:'10px', color:'#cccccc', lineBreak:'anywhere', wordBreak:'normal', overflow:'hidden', whiteSpace:'nowrap', textOverflow:'ellipsis', fontFamily:'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight:'100'}}>
+            <a href="https://soundcloud.com/1bran" title="bran" target="_blank" style={{color: "#cccccc", textDecoration: 'none'}}>
+              bran
+            </a>
+             · 
+            <a href="https://soundcloud.com/1bran/sets/my-favs" title="my favs" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>
+              my favs
+            </a>
+          </div>
         </div>
       </div>
     </section>
