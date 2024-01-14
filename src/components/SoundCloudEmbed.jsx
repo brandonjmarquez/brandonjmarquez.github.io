05f1c5287bdcbd4f8c2737b75f239c2af9cf8ca8
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2024-01-14 01:06:22 -0600

    Changed color for SoundCloud embed.

--- src/components/SoundCloudEmbed.jsx
+++ src/components/SoundCloudEmbed.jsx
@@ -1,7 +1,7 @@
 const SoundCloudEmbed = () => {
   return (
     <div className="self-center sm:w-1/2">
-      <iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1754747808&color=%23bd00ff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+      <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1754747808&color=%23ee7185&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
       <div style={{fontSize:'10px', color:'#cccccc', lineBreak:'anywhere', wordBreak:'normal', overflow:'hidden', whiteSpace:'nowrap', textOverflow:'ellipsis', fontFamily:'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight:'100'}}>
         <a href="https://soundcloud.com/1bran" title="bran" target="_blank" style={{color: "#cccccc", textDecoration: 'none'}}>
           bran
