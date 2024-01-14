947a4a1f94306b92e846d1d74ef38879c67e591b
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2024-01-14 00:58:51 -0600

    Arrow navs are a bit more responsive. They are in a lower position for smaller devices.

--- src/components/Navigation/ArrowNavs.jsx
+++ src/components/Navigation/ArrowNavs.jsx
@@ -4,8 +4,8 @@ import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 const ArrowNavs = (props) => {
   return (
     <div>
-      <button className="fixed left-[3%] top-1/2 bg-transparent text-custom-300 rounded-full p-0 z-50" onClick={() => props.handleLeftMove()}><FaArrowCircleLeft className="text-3xl" /></button>
-      <button className="fixed right-[3%] top-1/2 bg-transparent text-custom-300 rounded-full p-0 z-50" onClick={() => props.handleRightMove()}><FaArrowCircleRight className="text-3xl" /></button>
+      <button className="fixed left-[3%] sm:top-1/2 top-3/4 bg-transparent text-custom-300 rounded-full p-0 z-50" onClick={() => props.handleLeftMove()}><FaArrowCircleLeft className="text-3xl" /></button>
+      <button className="fixed right-[3%] sm:top-1/2 top-3/4 bg-transparent text-custom-300 rounded-full p-0 z-50" onClick={() => props.handleRightMove()}><FaArrowCircleRight className="text-3xl" /></button>
     </div>
   );
 };
