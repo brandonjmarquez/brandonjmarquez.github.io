b710a231690f4b29e31e5bad37ec1ba31e8fe271
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2024-01-14 18:58:25 -0600

    Added hover animations to header buttons.

--- src/components/Navigation/Header.jsx
+++ src/components/Navigation/Header.jsx
@@ -6,7 +6,7 @@ function Header(props) {
     <nav id='navbar' className='sticky sm:w-3/4 w-full sm:top-[5vh] top-[2vh] sm:left-[12.5%] left-[0] sm:mb-[0%] mb-[15%] mx-auto z-50 shadow rounded-full bg-custom-200'>
       <div className='m-auto py-2 w-full text-white whitespace-nowrap z-50 shadow-md'>
         <div className=''>
-          <div className='flex items-center justify-evenly'>
+          <div className='header-container flex items-center justify-evenly'>
             <HeaderButton href="/about-me" label="About Me" pageOrder={props.pageOrder} currentPage={props.currentPage} tab={props.tab} setTab={props.setTab} handleLeftMove={props.handleLeftMove} handleRightMove={props.handleRightMove} />
             <HeaderButton href="/projects" label="Projects" pageOrder={props.pageOrder} currentPage={props.currentPage} tab={props.tab} setTab={props.setTab} handleLeftMove={props.handleLeftMove} handleRightMove={props.handleRightMove} />
             <HeaderButton href="/resume" label="Resume" pageOrder={props.pageOrder} currentPage={props.currentPage} tab={props.tab} setTab={props.setTab} handleLeftMove={props.handleLeftMove} handleRightMove={props.handleRightMove} />
