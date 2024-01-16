db3bb341760e037443d57073eca7aaa551702b2a
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2024-01-16 13:56:54 -0600

    Actually changed header to fixed.

--- src/components/Navigation/Header.jsx
+++ src/components/Navigation/Header.jsx
@@ -3,7 +3,7 @@ import HeaderButton from "../Buttons/HeaderButtons";
 
 function Header(props) {
   return (
-    <nav id='navbar' className='sticky sm:w-3/4 w-full sm:top-[5vh] top-[2vh] sm:left-[12.5%] left-[0] sm:mb-[0%] mb-[15%] mx-auto z-50 shadow rounded-full bg-custom-200'>
+    <nav id='navbar' className='fixed sm:w-3/4 w-full sm:top-[5vh] top-[2vh] sm:left-[12.5%] left-[0] sm:mb-[0%] mb-[15%] mx-auto z-50 shadow rounded-full bg-custom-200'>
       <div className='m-auto py-2 w-full text-white whitespace-nowrap z-50 shadow-md'>
         <div className=''>
           <div className='header-container flex items-center justify-evenly'>
